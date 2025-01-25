af5b7f70f81d26c517f0889873b571061ad01862
Fe! <cp176823@gmail.com>
2025-01-25 04:45:54 -0600

    Update objects.c

--- src/gfx/objects.c
+++ src/gfx/objects.c
@@ -21,16 +21,4 @@ Scene_t create_scene(Object_t *objects, int numObjects) {
 	};
 	return scene;
 }
-
-void transform_object(Object_t object, mat4_t transform) {
-	for (int i = 0; i < object.numVertices; i++) {
-		v4_t vertex = {object.vertices[i].x, object.vertices[i].y, object.vertices[i].z, 1.0f};
-		v4_t transformed = {
-			transform[0][0] * vertex.x + transform[0][1] * vertex.y + transform[0][2] * vertex.z + transform[0][3] * vertex.w,
-			transform[1][0] * vertex.x + transform[1][1] * vertex.y + transform[1][2] * vertex.z + transform[1][3] * vertex.w,
-			transform[2][0] * vertex.x + transform[2][1] * vertex.y + transform[2][2] * vertex.z + transform[2][3] * vertex.w,
-			transform[3][0] * vertex.x + transform[3][1] * vertex.y + transform[3][2] * vertex.z + transform[3][3] * vertex.w
-		};
-		object.vertices[i] = (v3_t) {transformed.x, transformed.y, transformed.z};
-	}
-}
+// TODO: Add transformations
