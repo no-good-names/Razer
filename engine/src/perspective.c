dcf1dbf8de276ee89fc385367d9fffbc2f474b05
Bad name <67664504+no-good-names@users.noreply.github.com>
2025-08-11 13:18:22 -0500

    temp perspective

--- engine/src/perspective.c
+++ engine/src/perspective.c
@@ -2,4 +2,4 @@
 // Created by cp176 on 8/8/2025.
 //
 
-#include <Razer/native_camera.h>
+#include <Razer/perspective.h>
--- test/main.c
+++ test/main.c
@@ -6,11 +6,11 @@
 int main(int argc, char *argv[]) {
     init_video(800, 600, 1);
 
-    const vec3 v[4] = {
-        {-0.5f, 0.5f, 0.0f},
-        {0.5f, 0.5f, 0.0f},
-        {-0.5f, -0.5f, 0.0f},
-        {0.5f, -0.5f, 0.0f}
+    vec3 v[4] = {
+        {-0.5f, 0.5f, 0.0f}, // ftl 0
+        {0.5f, 0.5f, 0.0f}, // ftr 1
+        {-0.5f, -0.5f, 0.0f}, // fbl 2
+        {0.5f, -0.5f, 0.0f}, // fbr 3
     };
 
     ivec3 f[2] = {
@@ -24,11 +24,37 @@ int main(int argc, char *argv[]) {
     };
 
     set_render_mode(TRIANGLE_RASTERIZE);
+    float angle = 0.0f;
+
+    mat4 projection;
+    glm_perspective(glm_rad(60.0f), 800.0f/600.0f, 0.1f, 100.0f, projection);
+
     while (!quit) {
+        angle += 0.5f;
         update_events();
         // clear screen (pixel and z-buffer) every frame
         clear_screen();
-        render_triangle(v, f, 2, colors);
+
+        mat4 rotation, translation, scale, model, mout;
+        glm_scale_make(scale, (vec3) {1, 1, 1});
+        glm_rotate_make(rotation, glm_rad(angle), (vec3){0, 1, 0});
+        glm_translate_make(translation, (vec3){0.0f, 0.0f, 2.0f});
+        glm_mat4_mul(scale, rotation, model);
+        glm_mat4_mul(translation, model, model);
+        glm_mat4_mul(projection, model, mout);
+
+        // Apply rotation to vertices
+        vec3 projected_v[4];
+        for (int i = 0; i < 4; i++) {
+
+            vec4 temp = {v[i][0], v[i][1], v[i][2], 1.0f};
+            glm_mat4_mulv(mout, temp, temp); // transform
+            projected_v[i][0] = temp[0] / temp[3]; // perspective divide
+            projected_v[i][1] = temp[1] / temp[3];
+            projected_v[i][2] = temp[2] / temp[3];
+        }
+
+        render_triangle(projected_v, f, 2, colors);
 
         const uint8_t *key = SDL_GetKeyboardState(NULL);
         if (key[SDL_SCANCODE_ESCAPE]) {
